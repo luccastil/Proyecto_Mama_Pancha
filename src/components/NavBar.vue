a5e766e102eda0e6cd39df43b8260e384906734f
Jess <Jessjo.324@gmail.com>
2021-09-23 17:56:31 -0500

    Barra de navegación con links en funcionamiento

---
+++ src/components/NavBar.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <v-card color="grey lighten-4" flat height="200px" tile >
+            <v-toolbar extended>
+
+            <v-toolbar-title>
+                <h1 class="logo__nombre no-margin centrar-texto" >Mama<span class="logo__bold">Pancha</span></h1>
+            </v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <router-link class="link" to="/"><v-btn class="button" depressed elevation="3" plain> Productos </v-btn></router-link>
+            <router-link class="link" to="/contacto"><v-btn class="button" depressed elevation="3" plain> Contacto </v-btn></router-link>
+            <router-link class="link" to="/recomendados"><v-btn class="button" depressed elevation="3" plain> Recomendados </v-btn></router-link>
+            <router-view/>
+
+            </v-toolbar>
+        </v-card>
+    </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style>
+
+    h1 {
+        font-family: 'Brush Script MT', cursiva;
+        font-size: 3rem;
+        margin-top: 3rem;
+        font-weight: normal;
+    }
+
+    .button {
+        text-align: center;
+        margin: 1rem;
+        margin-top: 5rem;
+    }
+
+    .logo__bold { 
+        font-family: 'Calibri'; 
+        font-weight: bold;
+    }
+
+    .link {
+        text-decoration: none;
+    }
+
+</style>
---
+++ src/views/Recomendados.vue
@@ -0,0 +1,14 @@
+<template>
+    <h2>Recomendados</h2>
+    
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
